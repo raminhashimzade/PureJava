d6a93f7076817ed7bc751a733e4c484f67eaf95f
Ramin Hashimzade <ramin.hashimzade@gmail.com>
2022-06-03 12:16:01 +0400

    big decimal

--- PureJava/src/TopTalTest.java
+++ PureJava/src/TopTalTest.java
@@ -1,46 +1,54 @@
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class TopTalTest {
 
-	//-------------- (1)
-	private static List<Integer> getChange(float M, float P) {
+	// -------------- (1)
+	private static List<Integer> getChange(BigDecimal M, BigDecimal P) {
+
+		BigDecimal qaliq = M.subtract(P);
+
+		BigDecimal[] d = { BigDecimal.valueOf(1), 
+				BigDecimal.valueOf(0.5), 
+				BigDecimal.valueOf(0.25), 
+				BigDecimal.valueOf(0.1),
+				BigDecimal.valueOf(0.05), 
+				BigDecimal.valueOf(0.01) };
 		
-		float qaliq = M-P;
-		
-		float[] d = {(float)1,
-					 (float)0.5,
-					 (float)0.25,
-					 (float)0.1,
-					 (float)0.05,
-					 (float)0.01};
-		List<Integer> result = new ArrayList<Integer>(); 
-		
-			
+		List<Integer> result = new ArrayList<Integer>();
+
+		System.out.println("qaliq22 = " + qaliq);
 		for (int i = 0; i < d.length; i++) {
-			int dm = (int) Math.floor(qaliq/d[i]);			
-			qaliq = qaliq - dm*d[i];			
+
+			int dm = qaliq.divide(d[i], 2, RoundingMode.FLOOR).intValue();
+			System.out.println("di = "+d[i]+"    2.dm = " + dm);
+			
+			qaliq = qaliq.subtract(d[i].multiply(BigDecimal.valueOf(dm)));
+			System.out.println("qaliq = " + qaliq);
+			
+
 			result.add(dm);
-		}		
+		}
 		System.out.println(result);
-		
+
 		return result;
 	}
 
-	//-----------
+	// -----------
 	public static void main(String[] args) {
 
 		System.out.println("Hello World!");
 
 		Scanner scanner = new Scanner(System.in);
-		
-		float m = scanner.nextFloat();
-		float p = scanner.nextFloat();		
+
+		BigDecimal m = scanner.nextBigDecimal();
+		BigDecimal p = scanner.nextBigDecimal();
 		scanner.close();
 
 		getChange(m, p);
 	}
-	
-	
+
 }
